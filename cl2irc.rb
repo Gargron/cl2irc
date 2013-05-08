05041266547753532f39b562ee008f2a5016daa5
Eugen Rochko <eugen@zeonfederated.com>
2013-05-08 13:34:42 +0200

    Added some environment variables and some more logging to ease configuration and stuff. Also, different default IRC channel, because the official one is sort of locked for guests.

--- cl2irc.rb
+++ cl2irc.rb
@@ -6,47 +6,65 @@ require 'json'
 require 'logger'
 require 'cgi'
 
+irc_channel = ENV['IRC_CHANNEL'] || '#ColorlessTest'
+cl_channel  = ENV['CL_CHANNEL'] || 'main'
+
+# Hello, this is the IRC client
 irc_client = EventMachine::IRC::Client.new do
-  host   'irc.rizon.net'
-  port   '6667'
+  host   ENV['IRC_HOST'] || 'irc.rizon.net'
+  port   ENV['IRC_PORT'] || '6667'
 
   on :connect do
+    puts 'Connected to IRC'
+    user 'server_tan', 0, 'Server-tan'
     nick 'server_tan'
-    join "#Colorless"
+    join irc_channel
+  end
+
+  on :disconnect do
+    puts 'Disconnected from IRC'
+  end
+
+  on :parsed do |hash|
+    puts "#{hash[:prefix]} #{hash[:command]} #{hash[:params].join(' ')}"
   end
 end
 
 EM.run do
   q = EM::Queue.new
 
+  # This callback on the queue is used to push messages from the WebSocket client to the IRC client
   callback = Proc.new do |line|
-    irc_client.message("#Colorless", line)
+    irc_client.message(irc_channel, line)
     q.pop(&callback)
   end
 
   q.pop(&callback)
 
-  cl_connection = EventMachine::WebSocketClient.connect('ws://chat.thecolorless.net:1234/ws/main')
+  # This is the WebSocket client
+  cl_connection = EventMachine::WebSocketClient.connect("ws://chat.thecolorless.net:1234/ws/#{cl_channel}")
 
   cl_connection.callback do
-    puts "Connected"
+    puts "Connected to The Colorless Chat"
   end
 
   cl_connection.errback do |e|
-    puts "Error: #{e}"
+    puts "The Colorless Chat Error: #{e}"
   end
 
   cl_connection.stream do |msg|
     begin
-      parsed_msg = JSON.parse msg.encode("UTF-8")
-      q.push("#{parsed_msg['user']['name']}: #{CGI.unescapeHTML(parsed_msg['text'])}")
+      parsed_msg     = JSON.parse msg.encode("UTF-8")
+      unescaped_text = CGI.unescapeHTML parsed_msg['text']
+
+      q.push("#{parsed_msg['user']['name']}: #{unescaped_text}")
     rescue Exception => e
-      puts "Exception: #{e}"
+      puts "The Colorless Chat Exception: #{e}"
     end
   end
 
   cl_connection.disconnect do
-    puts "Disconnected"
+    puts "Disconnected from The Colorless Chat"
     EM::stop_event_loop
   end
 
